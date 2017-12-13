1ea60533bb5f76e9781b478c1f838d89776dc2d4
Rudi Starcevic <rudijs@users.noreply.github.com>
2017-12-13 18:34:25 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const os = require('os')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Viseo Hong Kong', hostname: os.hostname, nonce: Math.random()});
+  res.render('index', { title: 'Viseo Hong Kong Digital Innovation Labs', hostname: os.hostname, nonce: Math.random()});
 });
 
 module.exports = router;
